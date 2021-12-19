141660dc3161428e92cc2efde07963cbfe206c3f
Medhansh100 <84464193+Medhansh100@users.noreply.github.com>
2021-12-19 19:04:49 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,11 @@
+function adduser() {
+    user_name = document.getElementById("user_name").value;
+
+    localStorage.setItem("Username",user_name);
+
+    window.location = "kwitter_room.html";
+}
+
+function start(){
+    window.location = "kwitter_room.html";
+}
